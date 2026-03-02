001a0b684777e1fd2a7f7d8e63e5f799c5d6c568
SSONita <sarsovannita4@gmail.com>
2026-03-02 08:07:30 +0700

    HW-002 - Update AppSettingsState

--- ui/states/settings_state.dart
+++ ui/states/settings_state.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/widgets.dart';
- 
+import 'package:homework/w6/data/repositories/settings/app_settings_repository.dart';
+
 import '../../model/settings/app_settings.dart';
 
 class AppSettingsState extends ChangeNotifier {
- 
+  final AppSettingsRepository appSettingsRepository;
+
   AppSettings? _appSettings;
- 
-  AppSettingsState();
+
+  AppSettingsState(this.appSettingsRepository);
 
   Future<void> init() async {
     // Might be used to load data from repository
+    _appSettings = await appSettingsRepository.load();
   }
 
   ThemeColor get theme => _appSettings?.themeColor ?? ThemeColor.blue;
@@ -18,6 +21,7 @@ class AppSettingsState extends ChangeNotifier {
     if (_appSettings == null) return;
     _appSettings = _appSettings!.copyWith(themeColor: themeColor);
 
+    await appSettingsRepository.save(_appSettings!);
     notifyListeners();
   }
 }
